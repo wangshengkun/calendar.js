f53c824400df5c140294846e3a3431fe8309c116
王圣坤 <619435969@qq.com>
2017-10-09 20:06:38 +0800

    重构
    解决了跨浏览器事件处理函数的event丢失问题，将隐藏表单函数的内部逻辑理清且提炼为函数，不过本次重构造成了部分Bug，已找到根源所在，明天debug。

--- calendar.js
+++ calendar.js
@@ -86,9 +86,13 @@ function Calendar(){
 	this.date = this.now.getDate();
 
 	this.createCalendarFrame();
-	this.createHiddenTable();
-	this.getTimeDetail();
+	this.createHiddenYear();
+	this.createHiddenMonth();
+	this.jumpYear();
+	this.jumpMonth();
+	this.judgeYear();
 	this.render();
+	// this.init();
 
 	const title = document.getElementById("calendarTitle");
 	const self = this;
@@ -128,11 +132,11 @@ Calendar.prototype = {
 			for(let j = 0; j < 7; j++){
 				tbody.rows[i].insertCell(j);
 				//创建星期显示
-				if(i == 0){
+				if(i === 0){
 					tbody.rows[i].cells[j].innerHTML = week[j];
 				}
 				//如果是周末则添加样式
-				if(j == 5 || j == 6){
+				if(j === 5 || j === 6){
 					tbody.rows[i].cells[j].classList.add("weekend");
 				}
 			}
@@ -158,36 +162,49 @@ Calendar.prototype = {
 		document.body.appendChild(calendar);
 	},
 
-	//创建隐藏表单
-	createHiddenTable:function(){
+	//创建隐藏的年份选择表
+	createHiddenYear:function(){
 		const calendarTitle = document.getElementById("calendarTitle");
 		const yearTable = createNode("tbody", "yearTable");
-		const monthTable = createNode("tbody", "monthTable");
-		const self = this;
 
-		yearTable.classList.add("hide");
-		monthTable.classList.add("hide");
+		//该部分需提炼为函数
+		var year = this.year,
+			yearTxt = year.toString();
+		if(yearTxt[3] !== 0){
+			year = year - parseInt(yearTxt[3]);
+		}else{
+			year = year - 10;
+		}
 
-		//年份表单
+
 		for (let i = 0; i < 3; i++) {
 			yearTable.insertRow(i);
 			for (let j = 0; j < 5; j++) {
 				yearTable.rows[i].insertCell(j);
-				if(i == 0 && j == 0){
-					yearTable.rows[i].cells[j].innerHTML = "<";
-					yearTable.rows[i].cells[j].id = "preYear";
-				}else if(i == 0 && j == 4){
-					yearTable.rows[i].cells[j].innerHTML = ">";
-					yearTable.rows[i].cells[j].id = "nextYear";
-				}else if(i == 0 && (j != 0 || j != 4)){
+				if (i === 0 && (j !== 0 || j !== 4)) {
 					yearTable.rows[i].cells[j].classList.add("noSelect");
+				}
+				if(i >= 1){
+					yearTable.rows[i].cells[j].innerHTML = year++;
 				}
 			}
 		}
-		yearTable.rows[1].cells[0].id = "startYear";
-		yearTable.rows[2].cells[4].id = "endYear";	
 
-		//月份表单
+		yearTable.rows[0].cells[0].innerHTML = "<";
+		yearTable.rows[0].cells[0].id = "preYear";
+		yearTable.rows[0].cells[4].innerHTML = ">";
+		yearTable.rows[0].cells[4].id = "nextYear";
+		yearTable.rows[1].cells[0].id = "startYear";
+		yearTable.rows[2].cells[4].id = "endYear";
+
+		calendarTitle.appendChild(yearTable);
+		yearTable.classList.add("hide");
+	},
+
+	createHiddenMonth:function(){
+		const calendarTitle = document.getElementById("calendarTitle");
+		const monthTable = createNode("tbody", "monthTable");
+
 		var index = 1;
 		for(let i = 0; i < 3; i++){
 			monthTable.insertRow(i);
@@ -198,70 +215,97 @@ Calendar.prototype = {
 				index++;
 			}
 		}
-		calendarTitle.appendChild(yearTable);
+		
 		calendarTitle.appendChild(monthTable);
+		monthTable.classList.add("hide");
+	},
 
-		EventUtil.addHandler(yearTable, "click", function(event){
+	jumpYear:function(){
+		const yearTable = document.getElementById("yearTable");
+		const self = this;
+
+		function changeYear(event){  //隔离应用逻辑
 			const preYear = document.getElementById("preYear");
 			const nextYear = document.getElementById("nextYear");
 			const startYear = document.getElementById("startYear");
 			const endYear = document.getElementById("endYear");
+
 			event = EventUtil.getEvent(event);
 			var target = EventUtil.getTarget(event);
-				if(target == preYear){		
-					const index = startYear.value - 10;
-					self.refreshYear(index);
-				}else if(target == nextYear){
-					const index = endYear.value + 1;
-					self.refreshYear(index);
-				}else{
-					self.year = target.value;
-					yearTable.classList.add("hide");
-				}
-			});
+			if(target === preYear){
+				const index = startYear.value - 10;
+				self.refreshYear(index);
+			}else if(target === nextYear) {
+				const index = endYear.value + 1;
+				self.refreshYear(index);
+			}else{
+				self.year = target.value;
+				yearTable.classList.add("hide");
+			}
+		}
 
-		EventUtil.addHandler(monthTable, "click", function(event){
+		EventUtil.addHandler(yearTable, "click", function(event){
+			changeYear(event);
+		});
+	},
+
+	jumpMonth:function(){
+		const monthTable = document.getElementById("monthTable");
+		const self = this;
+
+		function changeMonth(event){
 			event = EventUtil.getEvent(event);
 			var target = EventUtil.getTarget(event);
 			self.month = target.value;
 			monthTable.classList.add("hide");
-		});	
+		}
+
+		EventUtil.addHandler(monthTable, "click", function(event){
+			changeMonth(event);
+		});
 	},
 
-	//获取时间细节
-	getTimeDetail:function(){
+	//判断平/闰年
+	judgeYear:function(){
 		const commonYear = [31,28,31,30,31,30,31,31,30,31,30,31];
 		const leapYear = [31,29,31,30,31,30,31,31,30,31,30,31];
-		this.flag = false;
-		this.tempDate = new Date(this.year, this.month-1);//当前年月所得到的时间
-		this.tempYear = this.tempDate.getFullYear();
-		this.tempMonth = this.tempDate.getMonth();
-		this.tempDay = this.tempDate.getDay();//该月份的第一天是星期几
-		if(this.tempDay == 0){
-			this.tempDay = 7;
+
+		function isLeapYear(){
+			return this.year % 400 === 0 || (this.year % 4 === 0 && this.yrar % 100 !== 0);
 		}
-		//判断是否为闰年
-		if(this.tempYear % 400 == 0 ||(this.tempYear % 4 == 0 && this.tempYear % 100 != 0)){
-			this.flag = true;
-		}
-		if(!this.flag){//平年
-			this.allDays = commonYear[this.tempMonth];
-		}else{//闰年
-			this.allDays = leapYear[this.tempMonth];
+
+		if(isLeapYear()){//闰年
+			this.allDays = leapYear[this.month-1];
+		}else{//平年
+			this.allDays = commonYear[this.month-1];
 		}
 	},
 
 	//渲染函数
 	render:function(){
-		//执行初始化函数
-		this.init();
+		//清空表格中的日期显示
+		(function(){
+			const tbody = document.getElementById("calendarBody");
+			for (let i = 1; i < 7; i++) {
+				for (let j = 0; j < 7; j++) {
+					tbody.rows[i].cells[j].innerHTML = "";
+					tbody.rows[i].cells[j].classList.remove("today");
+				}
+			}
+		})();
 
-		var index = 1;
+		const tempDate = new Date(this.year, this.month-1);
+		var	day = tempDate.getDay();
+		if(day === 0){
+			day = 7;
+		}
+
 		const tbody = document.getElementById("calendarBody");
 		const titleYear = document.getElementById("titleYear");
 		const titleMonth = document.getElementById("titleMonth");
+		var index = 1;
 
-		for (let i = this.tempDay-1; i < 7; i++) {
+		for (let i = day-1; i < 7; i++) {
 			tbody.rows[1].cells[i].innerHTML = index++;
 		}
 		for (let j = 2; j < 7; j++) {
@@ -271,29 +315,18 @@ Calendar.prototype = {
 				}
 			}
 		}
-		titleYear.firstChild.nodeValue = this.tempYear;
-		titleMonth.firstChild.nodeValue = (this.tempMonth+1);
+
+		titleYear.firstChild.nodeValue = this.year;
+		titleMonth.firstChild.nodeValue = this.month;
 
 		//如果与当前日期相同，则添加样式
 		for(let i = 1; i < 7; i++){
 			for(let j = 0; j < 7; j++){
-				if(this.tempYear == this.now.getFullYear() && (this.tempMonth+1) == this.now.getMonth()+1){
-					if(tbody.rows[i].cells[j].innerHTML == this.date){
+				if(this.year === this.now.getFullYear() && (this.month) === this.now.getMonth()+1){
+					if(parseInt(tbody.rows[i].cells[j].innerHTML) === this.date){
 						tbody.rows[i].cells[j].classList.add("today");
 					}
-				}else{
-					tbody.rows[i].cells[j].classList.remove("today");
 				}
-			}
-		}
-	},
-
-	//初始化函数，主要是清空表格中的日期显示
-	init:function(){
-		const tbody = document.getElementById("calendarBody");
-		for(let i = 1; i < 7; i++){
-			for(let j = 0; j < 7; j++){
-				tbody.rows[i].cells[j].innerHTML = "";
 			}
 		}
 	},
@@ -306,7 +339,8 @@ Calendar.prototype = {
 		const titleMonth = document.getElementById("titleMonth");
 		const yearTable = document.getElementById("yearTable");
 		const monthTable = document.getElementById("monthTable");
-		if(target == pre){
+
+		if(target === pre){
 			switch(this.month){
 				case 1:
 					this.year = --this.year;
@@ -315,7 +349,7 @@ Calendar.prototype = {
 				default:
 					this.month = --this.month;
 			}
-		}else if(target == next){
+		}else if(target === next){
 			switch(this.month){
 				case 12:
 					this.month = 1;
@@ -324,20 +358,21 @@ Calendar.prototype = {
 				default:
 					this.month = ++this.month;
 			}
-		}else if(target == titleYear && monthTable.classList.contains("hide")){
-			this.judgeYear();
-		}else if(target == titleMonth  && yearTable.classList.contains("hide")){
+		}else if(target === titleYear && monthTable.classList.contains("hide")){
+			// this.unsolved();
+			yearTable.classList.toggle("hide");
+		}else if(target === titleMonth  && yearTable.classList.contains("hide")){
 			monthTable.classList.toggle("hide");
 		}
 
 		titleYear.firstChild.nodeValue = this.year;
 		titleMonth.firstChild.nodeValue = this.month;
-		this.getTimeDetail();
+		this.judgeYear();
 		this.render();
 	},
 
-	//判断当前年份
-	judgeYear:function(){
+	//判断当前年份,该函数需重构
+	unsolved:function(){
 		const yearTable = document.getElementById("yearTable");
 		var index;
 
@@ -356,6 +391,7 @@ Calendar.prototype = {
 			//利用return语句停止执行剩余语句
 			return;
 		}
+		console.log(index);
 			this.refreshYear(index);
 			yearTable.classList.toggle("hide");	
 	},
